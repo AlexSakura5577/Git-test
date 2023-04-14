1bb4440c9e1044e285ad85ef64ecd95469473b14
Manticora <999manticora999@gmail.com>
2023-04-14 14:40:09 +0300

    feat: function setinterval not work

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-document.addEventListener("click", changeColor, false);
-function changeColor() {
-    document.body.style.backgroundColor = "#FFC926";
-}
+// document.addEventListener("click", changeColor, false);
+// function changeColor() {
+//     document.body.style.backgroundColor = "#FFC926";
+// }
+
+setInterval(function () {
+    $color = generateColor();
+    $('body')
+        .css('background-color', $color)
+        .text($color);
+}, 2000);
+
+// Generates random color 
+function generateColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+};
