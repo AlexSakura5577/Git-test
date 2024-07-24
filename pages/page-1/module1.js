89ad8193d2fa8103cdf1312c1699fa2ffab9c2b2
Manticora <999manticora999@gmail.com>
2024-07-24 18:59:44 +0300

    feat: start unit tests

--- pages/page-1/module1.js
+++ pages/page-1/module1.js
@@ -17,6 +17,4 @@ eventEmitter.subscribe('dataProcessed', (processedData) => {
     console.log(`Модуль 1 получил обработанные данные: ${processedData}`);
 });
 
-// Тестирование нового функционала
-eventEmitter.dispatch('userLogin', 'Александр'); // Это обработается
-eventEmitter.dispatch('userLogin', 'Ирина'); // Это уже не обработается
+
